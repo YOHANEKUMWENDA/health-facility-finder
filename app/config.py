16da0862780cd61fbb9b6571ea3db73ea5c3fe16
SydneyMtima <bsc-com-22-22@unima.ac.mw>
2025-11-27 14:43:08 +0200

    database configuration

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+
+#DATABASE CONFIGURATION
+class Config:
+    DB_CONFIG = {
+        'dbname': 'health_facilities_db',
+        'user': 'progress',
+        'password': 'dayire',
+        'host': 'localhost',
+        'port': 5432
+    }
